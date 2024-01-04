0d1e39199851dfcebe25d450e13368bbd798e044
Nurgali <117362075+NurGuru@users.noreply.github.com>
2024-01-04 19:21:03 +0300

    RA_03 create buttons

--- app/src/main/java/ru/nurguru/recipesapp/MainActivity.kt
+++ app/src/main/java/ru/nurguru/recipesapp/MainActivity.kt
@@ -2,10 +2,14 @@ package ru.nurguru.recipesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.nurguru.recipesapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
