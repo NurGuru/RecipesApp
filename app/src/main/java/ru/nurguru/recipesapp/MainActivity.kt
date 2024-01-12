5c4bb99996194550b45c8fb9ccf9be30bf62768e
Nurgali <117362075+NurGuru@users.noreply.github.com>
2024-01-12 13:14:19 +0300

    Ra-04 created categories fragment

--- app/src/main/java/ru/nurguru/recipesapp/MainActivity.kt
+++ app/src/main/java/ru/nurguru/recipesapp/MainActivity.kt
@@ -3,13 +3,18 @@ package ru.nurguru.recipesapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ru.nurguru.recipesapp.databinding.ActivityMainBinding
+import ru.nurguru.recipesapp.databinding.FragmentListCategoriesBinding
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().add(R.id.mainContainer,CategoriesListFragment()).commit()
+
     }
 }
