aff18962b98240dee8e2d37ce262c58700ac5975
Nurgali <117362075+NurGuru@users.noreply.github.com>
2024-01-17 19:02:35 +0300

    Ra-05 mistakes corrected

--- app/src/main/java/ru/nurguru/recipesapp/CategoriesListFragment.kt
+++ app/src/main/java/ru/nurguru/recipesapp/CategoriesListFragment.kt
@@ -7,14 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.nurguru.recipesapp.databinding.FragmentListCategoriesBinding
 
-class CategoriesListFragment: Fragment(R.layout.fragment_list_categories) {
-    private lateinit var binding: FragmentListCategoriesBinding
+class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
+    private var _binding: FragmentListCategoriesBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for FragmentListCategoriesBinding must not be null")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentListCategoriesBinding.inflate(inflater)
+    ): View {
+        _binding = FragmentListCategoriesBinding.inflate(inflater)
         return binding.root
     }
 }
--- app/src/main/java/ru/nurguru/recipesapp/MainActivity.kt
+++ app/src/main/java/ru/nurguru/recipesapp/MainActivity.kt
@@ -6,27 +6,29 @@ import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
 import ru.nurguru.recipesapp.databinding.ActivityMainBinding
-import ru.nurguru.recipesapp.databinding.FragmentListCategoriesBinding
-
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private var _binding: ActivityMainBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityMainBinding must not be null")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportFragmentManager.commit {
             setReorderingAllowed(true)
             add<CategoriesListFragment>(R.id.mainContainer)
+            addToBackStack(null)
         }
 
         binding.navBtnFavorite.setOnClickListener {
             supportFragmentManager.commit {
                 replace<FavoritesFragment>(R.id.mainContainer)
                 setReorderingAllowed(true)
-                addToBackStack("null")
+                addToBackStack(null)
             }
         }
 
@@ -34,7 +36,7 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.commit {
                 replace<CategoriesListFragment>(R.id.mainContainer)
                 setReorderingAllowed(true)
-                addToBackStack("null")
+                addToBackStack(null)
             }
         }
     }
